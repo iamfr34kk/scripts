64ab5985cb95f6d839c6d504720351c828ee52e0
Tameshwar Das Jangade <tameshwardas45@gmail.com>
2026-02-08 04:30:38 +0530

    Add files via upload

--- theming.sh
+++ theming.sh
@@ -1,2 +1,8 @@
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.background picture-uri-dark "file:///home/$(whoami)/setup/scripts/backgrounds/teal.jpg"
+gsettings set org.gnome.desktop.interface gtk-theme "Colloid-Teal-Dark-Compact"
+gsettings set org.gnome.desktop.interface icon-theme "Colloid-Teal-Dark"
+dconf write /org/gnome/shell/extensions/user-theme/name "'Colloid-Teal-Dark-Compact'"
+gsettings set org.gnome.desktop.interface font-name 'Gugi 10'
+gsettings set org.gnome.desktop.interface document-font-name 'Fira Code weight=450 10'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Fira Code weight=450 10'
